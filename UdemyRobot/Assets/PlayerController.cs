e5966c208243aab4cc7b0978c9f761ff30c344c9
Raul <raul.lopezm@gmail.com>
2022-01-15 22:58:13 +0100

    player moving and weapon aiming to mouse

---
+++ UdemyRobot/Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D playerRigidbody;
+    [SerializeField] Transform weaponsArm;
+    private Camera mainCamera;
+    [SerializeField] int movementSpeed;
+    private Vector2 movementInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+
+        playerRigidbody.velocity = movementInput * movementSpeed;
+
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 screenPoint = mainCamera.WorldToScreenPoint(transform.localPosition);
+
+        Vector2 offset = new Vector2(mousePosition.x - screenPoint.x, mousePosition.y - screenPoint.y);
+
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        weaponsArm.rotation = Quaternion.Euler(0, 0, angle);
+
+    }
+}
